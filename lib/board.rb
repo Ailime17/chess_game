1f96ca926418f497d7e00517ccbff4337085ce5a
Emilia <emilia.skrzypczak.es@gmail.com>
2022-12-30 13:50:03 +0100

    add board.rb

---
+++ lib/board.rb
@@ -0,0 +1,97 @@
+require_relative 'players'
+
+# class that sets up the board
+class Board
+  attr_reader :board, :board_for_display
+
+  def initialize
+    @white = PlayerOne.new
+    @black = PlayerTwo.new
+    @board = make_board
+    @board_for_display = make_board_for_display
+  end
+
+  # files are columns & ranks are rows on the board
+  def make_board
+    board = Hash.new(0)
+    'a'.upto('h') do |file|
+      1.upto(8) do |rank|
+        if (3..6).include?(rank)
+          board[[file, rank]] = ''
+        else
+          place_a_piece(board, file, rank)
+        end
+      end
+    end
+    board
+  end
+
+  def place_a_piece(board, file, rank)
+    board[[file, rank]] =
+      case rank
+      when 2
+        @white.pawn
+      when 7
+        @black.pawn
+      when 1
+        if %w[a h].include?(file)
+          @white.rook
+        elsif %w[b g].include?(file)
+          @white.knight
+        elsif %w[c f].include?(file)
+          @white.bishop
+        elsif file == 'd'
+          @white.queen
+        elsif file == 'e'
+          @white.king
+        end
+      when 8
+        if %w[a h].include?(file)
+          @black.rook
+        elsif %w[b g].include?(file)
+          @black.knight
+        elsif %w[c f].include?(file)
+          @black.bishop
+        elsif file == 'd'
+          @black.queen
+        elsif file == 'e'
+          @black.king
+        end
+      end
+  end
+
+  def make_board_for_display
+    board = %{
+         ________________________________________
+        |                                       |\\
+      8 | br | bn | bb | bq | bk | bb | bn | br ||
+        |---------------------------------------||
+      7 | bp | bp | bp | bp | bp | bp | bp | bp ||
+        |---------------------------------------||
+      6 |    |    |    |    |    |    |    |    ||
+        |---------------------------------------||
+      5 |    |    |    |    |    |    |    |    ||
+        |---------------------------------------||
+      4 |    |    |    |    |    |    |    |    ||
+        |---------------------------------------||
+      3 |    |    |    |    |    |    |    |    ||
+        |---------------------------------------||
+      2 | wp | wp | wp | wp | wp | wp | wp | wp ||
+        |---------------------------------------||
+      1 | wr | wn | wb | wq | wk | wb | wn | wr ||
+        |_______________________________________||
+         \\______________________________________\\|
+           a    b    c    d    e    f    g    h
+    }
+    place_pieces_on_board(board)
+    board
+  end
+
+  def place_pieces_on_board(board)
+    [['wq',@white.queen], ['bq',@black.queen], ['wk',@white.king], ['bk',@black.king],
+     ['wr',@white.rook], ['br',@black.rook], ['wn',@white.knight], ['bn',@black.knight],
+     ['wp',@white.pawn], ['bp',@black.pawn], ['wb',@white.bishop], ['bb',@black.bishop]].each do |pair|
+      board.gsub!(pair[0], "#{pair[1]} ")
+    end
+  end
+end
