6d61892ba337377fa93739ffeb28458877935c90
Emilia <emilia.skrzypczak.es@gmail.com>
2022-12-31 18:18:45 +0100

    add allowed_moves

--- lib/rook.rb
+++ lib/rook.rb
@@ -8,4 +8,67 @@ class RookMoves
   def equals_unicode_piece?(unicode_piece)
     @visual_pieces.include?(unicode_piece)
   end
+
+  def allowed_moves(start_square, end_square, _player, board)
+    @board = board
+    # lazy version, because allowed_moves doesn't include all possible end_squares, only the one end_square if legal
+    allowed_moves = []
+    allowed_moves << end_square if legal_and_empty_path?(start_square, end_square)
+    allowed_moves
+  end
+
+  def legal_and_empty_path?(start_square, end_square)
+    start_file = start_square[0]
+    end_file = end_square[0]
+    start_rank = start_square[1]
+    end_rank = end_square[1]
+    if start_rank == end_rank
+      return false unless horizontal_path_empty?(start_square, end_square)
+    elsif start_file == end_file
+      return false unless vertical_path_empty?(start_square, end_square)
+    else
+      return false
+    end
+    true
+  end
+
+  def horizontal_path_empty?(start_square, end_square)
+    start_file = start_square[0]
+    end_file = end_square[0]
+    add_or_substract = if start_file < end_file
+                         :+
+                       elsif start_file > end_file
+                         :-
+                       end
+    square = start_square
+    square[0] = (square[0].ord.public_send(add_or_substract, 1)).chr
+    until square == end_square
+      return false unless square_empty?(square)
+
+      square[0] = (square[0].ord.public_send(add_or_substract, 1)).chr
+    end
+    true
+  end
+
+  def vertical_path_empty?(start_square, end_square)
+    start_rank = start_square[1]
+    end_rank = end_square[1]
+    add_or_substract = if start_rank < end_rank
+                         :+
+                       elsif start_rank > end_rank
+                         :-
+                       end
+    square = start_square
+    square[1] = square[1].public_send(add_or_substract, 1)
+    until square == end_square
+      return false unless square_empty?(square)
+
+      square[1] = square[1].public_send(add_or_substract, 1)
+    end
+    true
+  end
+
+  def square_empty?(square)
+    @board[square].empty?
+  end
 end
