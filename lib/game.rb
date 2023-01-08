bf8cfdf0d39e7161b9aae1bd9076d3b2719e9c12
Emilia <emilia.skrzypczak.es@gmail.com>
2023-01-08 19:51:59 +0100

    game.rb: Move methods around to make the class more readable

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,7 @@ require_relative 'knight'
 require_relative 'pawn'
 require_relative 'queen'
 require_relative 'king'
+# require_relative 'special_moves'
 
 # class for the command line chess game
 class ChessGame
@@ -18,6 +19,7 @@ class ChessGame
     @bishop = BishopMoves.new
     @queen = QueenMoves.new
     @king = KingMoves.new
+    # @special_moves = SpecialMoves.new
   end
 
   def introduction
@@ -100,6 +102,29 @@ class ChessGame
       legal_move?(start_square, end_square, player)
   end
 
+  def square_empty?(square, board = @board)
+    board[square].empty?
+  end
+
+  # checks if a piece belongs to the player whose turn it currently is
+  def piece_belongs_to_player?(square, player, board = @board)
+    piece = board[square]
+    player.player_pieces.key?(piece)
+  end
+
+  def legal_move?(start_square, end_square, player)
+    piece = read_piece_name(start_square)
+    piece.allowed_moves(start_square, end_square, player, @board).include?(end_square) ||
+      castling_move?(start_square, end_square, player, piece) ||
+      en_passant?(start_square, end_square, player, piece)
+  end
+
+  def read_piece_name(square, board = @board)
+    unicode_piece = board[square]
+    pieces = [@knight, @pawn, @rook, @bishop, @queen, @king]
+    pieces.each { |piece| return piece if piece.equals_unicode_piece?(unicode_piece) }
+  end
+
   def castling_move?(start_square, end_square, player, piece = read_piece_name(start_square))
     piece == @king &&
       !king_is_in_check?(player) &&
@@ -109,6 +134,20 @@ class ChessGame
       path_between_king_and_rook_empty?(start_square, end_square, player) &&
       !king_will_be_in_check_during_castling?(start_square, end_square, player) &&
       !king_will_be_in_check_after_castling?(start_square, end_square, player)
+  end
+
+  def king_is_in_check?(player, board = @board)
+    king_unicode = player.king
+    king_square = board.key(king_unicode)
+    opponent = (player == @player1 ? @player2 : @player1)
+
+    board.each_key do |square|
+      next if square_empty?(square, board) || piece_belongs_to_player?(square, player, board)
+
+      piece = read_piece_name(square, board)
+      return true if piece.allowed_moves(square, king_square, opponent, board).include?(king_square)
+    end
+    false
   end
 
   def king_moves_2_places_horizontally?(start_square, end_square)
@@ -124,6 +163,10 @@ class ChessGame
     else
       player.rook_a_moved == false
     end
+  end
+
+  def king_moved_towards_rook_h?(start_square, end_square)
+    end_square[0] > start_square[0]
   end
 
   def path_between_king_and_rook_empty?(start_square, end_square, player)
@@ -164,6 +207,16 @@ class ChessGame
     false
   end
 
+  def move_puts_the_king_in_check?(start_square, end_square, player)
+    mock_board = Hash.new(0)
+    @board.each do |square, piece|
+      mock_board[square] = piece
+    end
+    mock_board[end_square] = mock_board[start_square]
+    mock_board[start_square] = ''
+    king_is_in_check?(player, mock_board)
+  end
+
   def king_will_be_in_check_after_castling?(king_square, end_square, player)
     mock_board = Hash.new(0)
     @board.each do |square, piece|
@@ -185,27 +238,35 @@ class ChessGame
     king_is_in_check?(player, mock_board)
   end
 
-  def square_empty?(square, board = @board)
-    board[square].empty?
+  def en_passant?(start_square, end_square, player, player_piece = read_piece_name(start_square))
+    return false if @most_recent_move.nil?
+
+    @most_recent_start_square = [@most_recent_move[0], @most_recent_move[1].to_i]
+    @most_recent_end_square = [@most_recent_move[2], @most_recent_move[3].to_i]
+
+    player_piece == @pawn &&
+      opponent_pawn_just_moved_two_places? &&
+      opponent_pawn_just_sat_next_to_player_pawn?(start_square) &&
+      player_pawn_moves_where_the_opponent_pawn_would_be_if_it_moved_one_place?(player, end_square)
   end
 
-  # checks if a piece belongs to the player whose turn it currently is
-  def piece_belongs_to_player?(square, player, board = @board)
-    piece = board[square]
-    player.player_pieces.key?(piece)
+  def opponent_pawn_just_moved_two_places?
+    start_rank = @most_recent_start_square[1]
+    end_rank = @most_recent_end_square[1]
+
+    read_piece_name(@most_recent_end_square) == @pawn &&
+      (end_rank == start_rank + 2 || end_rank == start_rank - 2)
   end
 
-  def legal_move?(start_square, end_square, player)
-    piece = read_piece_name(start_square)
-    piece.allowed_moves(start_square, end_square, player, @board).include?(end_square) ||
-      castling_move?(start_square, end_square, player, piece) ||
-      en_passant?(start_square, end_square, player, piece)
+  def opponent_pawn_just_sat_next_to_player_pawn?(start_square)
+    (@most_recent_end_square[0] == (start_square[0].ord + 1).chr ||
+      @most_recent_end_square[0] == (start_square[0].ord - 1).chr) &&
+      @most_recent_end_square[1] == start_square[1]
   end
 
-  def read_piece_name(square, board = @board)
-    unicode_piece = board[square]
-    pieces = [@knight, @pawn, @rook, @bishop, @queen, @king]
-    pieces.each { |piece| return piece if piece.equals_unicode_piece?(unicode_piece) }
+  def player_pawn_moves_where_the_opponent_pawn_would_be_if_it_moved_one_place?(current_player, end_square)
+    add_or_substract_opponent_rank = (current_player == @player1 ? :- : :+)
+    end_square == [@most_recent_start_square[0], @most_recent_start_square[1].public_send(add_or_substract_opponent_rank, 1)]
   end
 
   # (the order of methods is very important in make_move)
@@ -237,28 +298,82 @@ class ChessGame
     player_turn(player)
   end
 
-  def king_is_in_check?(player, board = @board)
-    king_unicode = player.king
-    king_square = board.key(king_unicode)
-    opponent = (player == @player1 ? @player2 : @player1)
-
-    board.each_key do |square|
-      next if square_empty?(square, board) || piece_belongs_to_player?(square, player, board)
-
-      piece = read_piece_name(square, board)
-      return true if piece.allowed_moves(square, king_square, opponent, board).include?(king_square)
-    end
-    false
+  def perform_en_passant(start_square, end_square, player, unicode_piece)
+    update_opponents_pieces(@most_recent_end_square, player)
+    # for the player pawn:
+    update_both_boards(start_square, end_square, unicode_piece)
+    # for the opponent pawn:
+    update_board(@most_recent_end_square, @most_recent_end_square, '')
+    update_board_for_display(@most_recent_end_square, @most_recent_end_square, ' ')
   end
 
-  def move_puts_the_king_in_check?(start_square, end_square, player)
-    mock_board = Hash.new(0)
-    @board.each do |square, piece|
-      mock_board[square] = piece
+  def update_opponents_pieces(end_square, player)
+    opponent = (player == @player1 ? @player2 : @player1)
+    unicode_piece = @board[end_square]
+    opponent.player_pieces[unicode_piece] -= 1
+  end
+
+  def update_both_boards(start_square, end_square, unicode_piece)
+    update_board(start_square, end_square, unicode_piece)
+    update_board_for_display(start_square, end_square, unicode_piece)
+  end
+
+  def update_board(start_square, end_square, unicode_piece)
+    @board[end_square] = unicode_piece
+    @board[start_square] = ''
+  end
+
+  def update_board_for_display(start_square, end_square, unicode_piece)
+    start_rank = start_square[1].to_s
+    start_file = start_square[0]
+    end_rank = end_square[1].to_s
+    end_file = end_square[0]
+    # empty the square where the piece was:
+    @board_for_display[@board_for_display.index(start_rank) + get_file_index(start_file)] = ' '
+    # place the piece on the new square:
+    @board_for_display[@board_for_display.index(end_rank) + get_file_index(end_file)] = unicode_piece
+  end
+
+  # for @board_for_display:
+  # reads the number of index places to add to the index of a particular rank, based on which file we want to visit
+  def get_file_index(file)
+    file_indexes = { 'a' => 4, 'b' => 9, 'c' => 14, 'd' => 19, 'e' => 24, 'f' => 29, 'g' => 34, 'h' => 39 }
+    file_indexes[file]
+  end
+
+  def perform_castling(start_square, end_square, player)
+    a_rook = (player == @player1 ? ['a', 1] : ['a', 8])
+    h_rook = (player == @player1 ? ['h', 1] : ['h', 8])
+    king_square = start_square
+    king_piece = @board[king_square]
+    if king_moved_towards_rook_h?(start_square, end_square)
+      rook_square = h_rook
+      square_next_to_king = [(king_square[0].ord + 1).chr, king_square[1]]
+    else
+      rook_square = a_rook
+      square_next_to_king = [(king_square[0].ord - 1).chr, king_square[1]]
     end
-    mock_board[end_square] = mock_board[start_square]
-    mock_board[start_square] = ''
-    king_is_in_check?(player, mock_board)
+    rook_piece = @board[rook_square]
+    update_both_boards(king_square, end_square, king_piece) # with king
+    update_both_boards(rook_square, square_next_to_king, rook_piece) # with rook
+    remember_moved_rook(rook_square, player)
+    remember_moved_king(player)
+  end
+
+  def remember_moved_rook(start_square, player)
+    file = start_square[0]
+    case file
+    when 'a' then player.rook_a_moved = true
+    when 'h' then player.rook_h_moved = true
+    end
+  end
+
+  def remember_moved_king(player)
+    player.king_moved = true
+  end
+
+  def makes_a_capture?(end_square)
+    !square_empty?(end_square)
   end
 
   def promotion?(end_square, player)
@@ -306,119 +421,6 @@ class ChessGame
       answer = gets.strip.downcase
     end
     answer
-  end
-
-  def perform_castling(start_square, end_square, player)
-    a_rook = (player == @player1 ? ['a', 1] : ['a', 8])
-    h_rook = (player == @player1 ? ['h', 1] : ['h', 8])
-    king_square = start_square
-    king_piece = @board[king_square]
-    if king_moved_towards_rook_h?(start_square, end_square)
-      rook_square = h_rook
-      square_next_to_king = [(king_square[0].ord + 1).chr, king_square[1]]
-    else
-      rook_square = a_rook
-      square_next_to_king = [(king_square[0].ord - 1).chr, king_square[1]]
-    end
-    rook_piece = @board[rook_square]
-    update_both_boards(king_square, end_square, king_piece) # with king
-    update_both_boards(rook_square, square_next_to_king, rook_piece) # with rook
-    remember_moved_rook(rook_square, player)
-    remember_moved_king(player)
-  end
-
-  def update_both_boards(start_square, end_square, unicode_piece)
-    update_board(start_square, end_square, unicode_piece)
-    update_board_for_display(start_square, end_square, unicode_piece)
-  end
-
-  def remember_moved_king(player)
-    player.king_moved = true
-  end
-
-  def remember_moved_rook(start_square, player)
-    file = start_square[0]
-    case file
-    when 'a' then player.rook_a_moved = true
-    when 'h' then player.rook_h_moved = true
-    end
-  end
-
-  def opponent_pawn_just_moved_two_places?
-    start_rank = @most_recent_start_square[1]
-    end_rank = @most_recent_end_square[1]
-
-    read_piece_name(@most_recent_end_square) == @pawn &&
-      (end_rank == start_rank + 2 || end_rank == start_rank - 2)
-  end
-
-  def opponent_pawn_just_sat_next_to_player_pawn?(start_square)
-    (@most_recent_end_square[0] == (start_square[0].ord + 1).chr ||
-      @most_recent_end_square[0] == (start_square[0].ord - 1).chr) &&
-      @most_recent_end_square[1] == start_square[1]
-  end
-
-  def player_pawn_moves_where_the_opponent_pawn_would_be_if_it_moved_one_place?(current_player, end_square)
-    add_or_substract_opponent_rank = (current_player == @player1 ? :- : :+)
-    end_square == [@most_recent_start_square[0], @most_recent_start_square[1].public_send(add_or_substract_opponent_rank, 1)]
-  end
-
-  def en_passant?(start_square, end_square, player, player_piece = read_piece_name(start_square))
-    return false if @most_recent_move.nil?
-
-    @most_recent_start_square = [@most_recent_move[0], @most_recent_move[1].to_i]
-    @most_recent_end_square = [@most_recent_move[2], @most_recent_move[3].to_i]
-
-    player_piece == @pawn &&
-      opponent_pawn_just_moved_two_places? &&
-      opponent_pawn_just_sat_next_to_player_pawn?(start_square) &&
-      player_pawn_moves_where_the_opponent_pawn_would_be_if_it_moved_one_place?(player, end_square)
-  end
-
-  def perform_en_passant(start_square, end_square, player, unicode_piece)
-    update_opponents_pieces(@most_recent_end_square, player)
-    # for the player pawn:
-    update_both_boards(start_square, end_square, unicode_piece)
-    # for the opponent pawn:
-    update_board(@most_recent_end_square, @most_recent_end_square, '')
-    update_board_for_display(@most_recent_end_square, @most_recent_end_square, ' ')
-  end
-
-  def makes_a_capture?(end_square)
-    !square_empty?(end_square)
-  end
-
-  def king_moved_towards_rook_h?(start_square, end_square)
-    end_square[0] > start_square[0]
-  end
-
-  def update_board(start_square, end_square, unicode_piece)
-    @board[end_square] = unicode_piece
-    @board[start_square] = ''
-  end
-
-  def update_opponents_pieces(end_square, player)
-    opponent = (player == @player1 ? @player2 : @player1)
-    unicode_piece = @board[end_square]
-    opponent.player_pieces[unicode_piece] -= 1
-  end
-
-  def update_board_for_display(start_square, end_square, unicode_piece)
-    start_rank = start_square[1].to_s
-    start_file = start_square[0]
-    end_rank = end_square[1].to_s
-    end_file = end_square[0]
-    # empty the square where the piece was:
-    @board_for_display[@board_for_display.index(start_rank) + get_file_index(start_file)] = ' '
-    # place the piece on the new square:
-    @board_for_display[@board_for_display.index(end_rank) + get_file_index(end_file)] = unicode_piece
-  end
-
-  # for @board_for_display:
-  # reads the number of index places to add to the index of a particular rank, based on which file we want to visit
-  def get_file_index(file)
-    file_indexes = { 'a' => 4, 'b' => 9, 'c' => 14, 'd' => 19, 'e' => 24, 'f' => 29, 'g' => 34, 'h' => 39 }
-    file_indexes[file]
   end
 end
 chess = ChessGame.new
