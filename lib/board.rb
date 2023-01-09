5f5c2e27f41a6417e49d13610c75da921e12ebdc
Emilia <emilia.skrzypczak.es@gmail.com>
2023-01-09 23:12:24 +0100

    board.rb: Transform the Board class into a Board module

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,7 @@
 require_relative 'players'
 
-# class that sets up the boards
-class Board
-  attr_reader :board, :board_for_display
-
-  def initialize
-    @white = PlayerOne.new
-    @black = PlayerTwo.new
-    @board = make_board
-    @board_for_display = make_board_for_display
-  end
-
+# module for the two types of boards, for creation & transformation of the boards
+module Board
   # files are columns & ranks are rows on the board
   def make_board
     board = Hash.new(0)
@@ -27,35 +18,37 @@ class Board
   end
 
   def place_a_piece(board, file, rank)
+    white = PlayerOne.new
+    black = PlayerTwo.new
     board[[file, rank]] =
       case rank
       when 2
-        @white.pawn
+        white.pawn
       when 7
-        @black.pawn
+        black.pawn
       when 1
         if %w[a h].include?(file)
-          @white.rook
+          white.rook
         elsif %w[b g].include?(file)
-          @white.knight
+          white.knight
         elsif %w[c f].include?(file)
-          @white.bishop
+          white.bishop
         elsif file == 'd'
-          @white.queen
+          white.queen
         elsif file == 'e'
-          @white.king
+          white.king
         end
       when 8
         if %w[a h].include?(file)
-          @black.rook
+          black.rook
         elsif %w[b g].include?(file)
-          @black.knight
+          black.knight
         elsif %w[c f].include?(file)
-          @black.bishop
+          black.bishop
         elsif file == 'd'
-          @black.queen
+          black.queen
         elsif file == 'e'
-          @black.king
+          black.king
         end
       end
   end
@@ -89,9 +82,11 @@ class Board
   end
 
   def place_pieces_on_board(board)
-    [['wq',@white.queen], ['bq',@black.queen], ['wk',@white.king], ['bk',@black.king],
-     ['wr',@white.rook], ['br',@black.rook], ['wn',@white.knight], ['bn',@black.knight],
-     ['wp',@white.pawn], ['bp',@black.pawn], ['wb',@white.bishop], ['bb',@black.bishop]].each do |pair|
+    white = PlayerOne.new
+    black = PlayerTwo.new
+    [['wq', white.queen], ['bq', black.queen], ['wk', white.king], ['bk', black.king],
+     ['wr', white.rook], ['br', black.rook], ['wn', white.knight], ['bn', black.knight],
+     ['wp', white.pawn], ['bp', black.pawn], ['wb', white.bishop], ['bb', black.bishop]].each do |pair|
       board.gsub!(pair[0], "#{pair[1]} ")
     end
   end
@@ -115,8 +110,29 @@ class Board
     black_squares.each do |square|
       rank = square[1].to_s
       file = square[0]
-      board[board.index(rank) + get_file_index(file)] = unicode_black_square if board[board.index(rank) + get_file_index(file)] == ' '
+      square_index = board.index(rank) + get_file_index(file)
+      board[square_index] = unicode_black_square if board[square_index] == ' '
     end
+  end
+
+  def update_board(start_square, end_square, unicode_piece)
+    @board[end_square] = unicode_piece
+    @board[start_square] = ''
+  end
+
+  def update_board_for_display(start_square, end_square, unicode_piece)
+    start_rank = start_square[1].to_s
+    start_file = start_square[0]
+    end_rank = end_square[1].to_s
+    end_file = end_square[0]
+    # empty the square where the piece was:
+    start_square_index = @board_for_display.index(start_rank) + get_file_index(start_file)
+    @board_for_display[start_square_index] = ' '
+    # place the piece on the new square:
+    end_square_index = @board_for_display.index(end_rank) + get_file_index(end_file)
+    @board_for_display[end_square_index] = unicode_piece
+
+    make_empty_black_squares_visibly_black(@board_for_display)
   end
 
   # for @board_for_display:
@@ -124,5 +140,10 @@ class Board
   def get_file_index(file)
     file_indexes = { 'a' => 4, 'b' => 9, 'c' => 14, 'd' => 19, 'e' => 24, 'f' => 29, 'g' => 34, 'h' => 39 }
     file_indexes[file]
+  end
+
+  def update_both_boards(start_square, end_square, unicode_piece)
+    update_board(start_square, end_square, unicode_piece)
+    update_board_for_display(start_square, end_square, unicode_piece)
   end
 end
